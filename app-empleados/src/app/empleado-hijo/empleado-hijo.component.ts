629a4034fa97fc4f2dc125465637e687286c00fc
hugojose14 <hugo.jose.1@hotmail.com>
2022-12-17 21:40:08 -0500

    @Input send data from parent component to child component

---
+++ app-empleados/src/app/empleado-hijo/empleado-hijo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo',
+  templateUrl: './empleado-hijo.component.html',
+  styleUrls: ['./empleado-hijo.component.css']
+})
+export class EmpleadoHijoComponent {
+
+  @Input() empleadoDeLista:Empleado;
+  @Input() indice: number;
+
+}
